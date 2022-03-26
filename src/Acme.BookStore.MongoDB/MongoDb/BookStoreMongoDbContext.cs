be90be4656c93e70b69ba720bf0d2d1fa6e7119a
Ahmet <yellow.dragon.cloud@gmail.com>
2022-03-26 23:38:54 +0300

    apply tutorial part 1

--- src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
+++ src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Volo.Abp.Data;
+﻿using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Volo.Abp.Data;
 using Volo.Abp.MongoDB;
 
 namespace Acme.BookStore.MongoDB;
@@ -9,6 +11,8 @@ public class BookStoreMongoDbContext : AbpMongoDbContext
     /* Add mongo collections here. Example:
      * public IMongoCollection<Question> Questions => Collection<Question>();
      */
+
+    public IMongoCollection<Book> Books => Collection<Book>();
 
     protected override void CreateModel(IMongoModelBuilder modelBuilder)
     {
